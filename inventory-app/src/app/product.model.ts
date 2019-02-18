403320e16c8dee803a1f6237da285bbddd655d00
Arkadiusz Lesica <S00182038@mail.itsligo.ie>
2019-02-18 23:52:50 +0000

    improvements

---
+++ inventory-app/src/app/product.model.ts
@@ -0,0 +1,12 @@
+/*
+Model of product object
+ */
+export class Product {
+  constructor(
+    public sku: string,
+    public name: string,
+    public imageUrl: string,
+    public department: string[],
+    public price: number) {
+  }
+}
